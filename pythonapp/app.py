0f78557c6aa77e35deabbc690d66dbefc5e1b21e
Payal Daki <130816609+payal-daki@users.noreply.github.com>
2024-07-20 18:09:02 +0530

    Delete pythonapp directory

--- pythonapp/app.py
+++
@@ -1,9 +0,0 @@
-import database
-
-def handle_get_posts():
-    return database.get_all_posts()
-
-def handle_add_post(title, content):
-    if not title or not content:
-        raise ValueError("Title and content cannot be empty")
-    database.add_post(title, content)
